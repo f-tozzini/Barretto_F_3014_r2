0d99d9bf4ed0dc2cf10804e57c5963d3b0aec6a6
f-tozzini <31866568+f-tozzini@users.noreply.github.com>
2018-02-21 21:30:02 -0500

    Create user from admin page

---
+++ admin/phpscripts/user.php
@@ -0,0 +1,22 @@
+<?php
+    function createUser($fname, $username, $password, $email, $lvllist) {
+      include('connect.php');
+
+      $userstring = "INSERT INTO tbl_user (user_fname, user_name, user_pass, user_email,
+        user_date, user_ip, user_level) VALUES ('${fname}', '${username}',
+        '${password}', '${email}', NOW(), 'no', '${lvllist}')";
+
+        // echo $userstring;
+        $userquery = mysqli_query($link, $userstring);
+        if($userquery) {
+          redirect_to('admin_index.php');
+        } else {
+          $message = "User error";
+          return $message;
+
+        }
+        mysqli_close($link);
+
+    }
+
+ ?>
